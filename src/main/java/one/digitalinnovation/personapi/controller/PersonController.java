9e859f3be430e81e63ef611ac000bcb1bcb7550c
Sandro Silva <sandro7lagoas@gmail.com>
2021-06-10 05:29:08 -0700

    01a

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @GetMapping
     public String getBook(){
-        return "API Test!";
+        return "Vai cagar Sergin!";
     }
 }
